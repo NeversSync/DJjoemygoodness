458a1908135322ae1bb86be17fc1403bddde8d88
Nevers <livelyevers@gmail.com>
2019-05-07 12:24:49 -0700

    Updates

---
+++ precache-manifest.26d42c6a21947719648508112d5b8def.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25f582016f74c6bd7af4",
+    "url": "/DJjoemygoodness/static/js/runtime~main.aed3ad26.js"
+  },
+  {
+    "revision": "6af537c38d951922b5b0",
+    "url": "/DJjoemygoodness/static/js/main.ba32e978.chunk.js"
+  },
+  {
+    "revision": "ef1cd84d41c578c69fc0",
+    "url": "/DJjoemygoodness/static/js/2.d55f4b58.chunk.js"
+  },
+  {
+    "revision": "6af537c38d951922b5b0",
+    "url": "/DJjoemygoodness/static/css/main.1a13de6d.chunk.css"
+  },
+  {
+    "revision": "c565e1e9a9e72d33e00d9dd7fa9afeb3",
+    "url": "/DJjoemygoodness/index.html"
+  }
+];
