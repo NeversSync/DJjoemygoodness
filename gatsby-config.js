4da8551e55a6d9f533edd7fef0eb7d9c29c1b4c1
Nevers <livelyevers@gmail.com>
2019-06-26 18:01:04 -0700

    Remove gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,25 +4,6 @@ module.exports = {
   },
   plugins: [
     // {
-    //   resolve: 'gatsby-plugin-favicon',
-    //   options: {
-    //     logo: './public/assets/img/tea-favicon.png',
-    //     // TODO
-    //     appName: null, // Inferred with your package.json
-    //     appDescription: null,
-    //     developerName: null,
-    //     developerURL: null,
-    //     dir: 'auto',
-    //     lang: 'en-US',
-    //     background: '#fff',
-    //     theme_color: '#fff',
-    //     display: 'standalone',
-    //     orientation: 'any',
-    //     start_url: '/?homescreen=1',
-    //     version: '1.0'
-    //   }
-    // },
-    // {
     //   // TODO
     //   resolve: 'gatsby-plugin-google-analytics',
     //   options: {
